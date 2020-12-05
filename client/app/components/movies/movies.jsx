ef98d5d32fc1092fb638f6e399e859a780218b30
Michael Mammoliti <mammoliti.michael@gmail.com>
2020-12-05 12:11:35 +0000

    tested the first api call

--- client/app/components/movies/movies.jsx
+++ client/app/components/movies/movies.jsx
@@ -1,39 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Movie, Row, Column, Container} from '@components';
-
-import data from '../../mocks/data';
 
 import styles from './movies.scss';
 
-const Movies = ({ movies, onMovieClick }) => {
+const Movies = ({ onMovieClick }) => {
+  const [movies, setMovies] = useState([]);
+  const [loadingState, setLoadingState] = useState([]);
+
   useEffect(() => {
-    fetch('/api/movie')
+    setLoadingState('pending');
+    fetch('/api/movies')
       .then(r => r.json())
       .then(r => {
-        console.log(r);
-      })
-      .catch(e => console.log(e));
-  });
+        setMovies(r);
+        setLoadingState('success');
+      });
+  }, []);
 
   return (
     <div className={styles['movies']}>
       <Container>
-        <Row spacing="m">
-          {movies.map((movie, movieIndex) => (
-            <Column p="1" t="3" d="4" key={movieIndex}>
-              <div key={movie.id} className={styles['movies__item']}>
-                <Movie {...movie} onClick={onMovieClick} />
-              </div>
-            </Column>
-          ))}
-        </Row>
+        {(loadingState === 'success') && (
+          <Row spacing="m">
+            {movies.map(movie => (
+              <Column p="1" t="3" d="4" key={movie['_id']}>
+                <div className={styles['movies__item']}>
+                  <Movie
+                    title={movie.title}
+                    stars={movie.stars}
+                    format={movie.format}
+                    releaseYear={movie.releaseYear}
+                    onClick={onMovieClick}
+                  />
+                </div>
+              </Column>
+            ))}
+          </Row>
+        )}
+        {loadingState === 'pending' && (
+          <p>loading</p>
+        )}
       </Container>
     </div>
   );
-};
-
-Movies.defaultProps = {
-  movies: data,
 };
 
 export default Movies;
