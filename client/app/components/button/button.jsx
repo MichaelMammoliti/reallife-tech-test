d3a4c9be50b75dba43cff8c6ab6195e245df515d
Michael Mammoliti <mammoliti.michael@gmail.com>
2020-12-06 20:13:53 +0000

    modified button component

--- client/app/components/button/button.jsx
+++ client/app/components/button/button.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './button.scss';
 
-const Button = ({ onClick, children }) => (
-  <button className={styles['button']} onClick={onClick} type="button">
+const Button = ({ type, children, onClick }) => (
+  <button className={styles['button']} type={type || 'button'} onClick={onClick}>
     {children}
   </button>
 );
 
-export default Button;
+const ButtonWrapper = ({
+  to,
+  type,
+  children,
+  onClick,
+}) => {
+  if (to) {
+    return (
+      <Link to={to} onClick={onClick}>
+        <Button type={type}>{children}</Button>
+      </Link>
+    );
+  }
+
+  return (
+    <Button type={type} onClick={onClick}>{children}</Button>
+  );
+};
+
+ButtonWrapper.defaultProps = {
+  type: 'button',
+  onClick: () => {},
+};
+
+export default ButtonWrapper;
