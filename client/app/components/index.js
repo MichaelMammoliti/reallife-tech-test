c0207c153d0a26d570d619c39355b32fa962017f
Michael Mammoliti <mammoliti.michael@gmail.com>
2020-12-06 20:11:12 +0000

    added containers main file and updated components main file

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -1,8 +1,25 @@
-export { default as Movies } from './movies';
-export { default as Movie } from './movie';
-export { default as Input } from './input';
-export { Row, Column } from './grid';
-export { default as Button } from './button';
+// UI Utils
 export { default as Container } from './container';
-export { default as Header } from './header';
+export { Row, Column } from './grid';
+
+// Inputs
+export { default as Input } from './input';
+export { default as Select } from './select';
+export { default as Button } from './button';
+
+// Components
+export { default as Section } from './section';
 export { default as Page } from './page';
+
+// Forms
+export { default as AddMovieForm } from './add-movie-form';
+
+// Movie
+export { default as MovieInfo } from './movie-info';
+export { default as Movies } from './movies';
+export { default as MovieList } from './movie-list';
+export { default as MovieListItem } from './movie-list-item';
+export { default as SearchBar } from './search-bar';
+export { default as Sidebar } from './sidebar';
+export { default as DefaultPageWrapper } from './default-page-wrapper';
+export { default as UploadMoviesForm } from './upload-movies-form';
---
+++ client/app/containers/index.js
@@ -0,0 +1,5 @@
+export { default as MoviesContainer } from './movies-container';
+export { default as AddMovieFormContainer } from './add-movie-form-container';
+export { default as SearchBarContainer } from './search-bar-container';
+export { default as MovieInfoContainer } from './movie-info-container';
+export { default as UploadMoviesFormContainer } from './upload-movies-form-container';
