0d19b3df60125e599f03790433e34e561e8bab65
Michael Mammoliti <mammoliti.michael@gmail.com>
2020-12-07 13:31:03 +0000

    fixed endpoint which was not working

--- client/app/containers/add-movie-form-container/add-movie-form-container.jsx
+++ client/app/containers/add-movie-form-container/add-movie-form-container.jsx
@@ -3,17 +3,20 @@ import { AddMovieForm } from '@components';
 
 const AddMovieFormContainer = () => {
   const [requestStatus, setRequestStatus] = useState('not-started');
-  const [formValues, setFormValues] = useState({});
+  const [formValues, setFormValues] = useState({ stars: '' });
 
   const handleOnSubmit = () => {
     setRequestStatus('pending');
 
     fetch('api/movies', {
       method: 'post',
-      body: {
-        ...formValues,
-        stars: formValues.stars.split(',').map(i => i.trim()).filter(Boolean),
+      headers: {
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify({
+        ...formValues,
+        stars: formValues.stars.split(',').map(item => item.trim()).filter(Boolean),
+      }),
     })
       .then(() => {
         setRequestStatus('success');
